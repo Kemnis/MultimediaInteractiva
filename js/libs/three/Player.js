e07baf0e3cccc22ec2daa7222cf96ff1b36ed04e
Unknown <verdeserpiente717@gmail.com>
2018-05-07 00:17:38 -0500

    Por implementar clase de movimientos
    Ya casi se puede implementar el movimiento de los personajes, solo falta crear una clase que cree los personajes con un constructor y luego hacer que se muevan con suavidad y vayan rotando o que solo avancen hacia el frente(Mas sencillo)

--- js/libs/three/Player.js
+++ js/libs/three/Player.js
@@ -1,69 +1,128 @@
 class Player{
     constructor(controllers){
-        this.control = controllers;
-        this.keyboard = new Keyboard();  
+        this.OTPlayer = controllers;
+        this.players = [];
+        this.playerControllers = [];
+        this.keyboard = new Keyboard();
         this.defaultController = new InputMapping(this.keyboard);
         this.ListObjs = new THREE.Object3D();
         this.renderer = new THREE.WebGLRenderer( {precision: "mediump" } );
         this.visibleSize =  { width: window.innerWidth, height: window.innerHeight};
-        this.clock = new THREE.Clock();		
-        this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, this.visibleSize.width / this.visibleSize.height, 0.1, 600);
+        this.clock = new THREE.Clock();	
+        this.scene = new THREE.Scene();
+        this.paused = false;
     }
 
-    sceneStart()
-    {
+    sceneStart(){
         this.loadOBJWithMTL("assets/", "planet1.obj", "planet1.mtl", (planet1) => {
-			this.SetObjectPos(planet1,0,-95,0);		
-			this.SetObjectRot(planet1,90,0,0);	
+			this.SetObjectPos(planet1,0,-95,0);
+			this.SetObjectRot(planet1,90,0,0);
 			this.SetObjectEsc(planet1,18,18,18);
 			this.scene.add(planet1);
 		});
 
 		this.loadOBJWithMTL("assets/", "enemy.obj", "enemy.mtl", (enemy) => {
-			this.SetObjectPos(enemy,0,0,-1);		
-			this.SetObjectRot(enemy,0,0,0);	
+			this.SetObjectPos(enemy,0,0,0);
+			this.SetObjectRot(enemy,0,0,0);
 			this.SetObjectEsc(enemy,0,0,0);
-			this.scene.add(enemy);
+			//this.scene.add(enemy);
 		});
 
         this.loadOBJWithMTL("assets/", "player.obj", "player.mtl", (player) => {
-            this.SetObjectPos(player,0,0,0);		
-            this.SetObjectRot(player,0,0,0);	
-            this.SetObjectEsc(player,0,0,0);
+            //this.SetObjectPos(player,0,0,0);
+            //this.SetObjectRot(player,0,0,0);
+            //this.SetObjectEsc(player,0,0,0);
             this.scene.add(player);
         });
+
+        this.loadOBJWithMTL("assets/", "player2.obj", "player2.mtl", (player2) => {
+            this.SetObjectPos(player2,0,0,10);
+            //this.SetObjectRot(player2,0,0,0);
+            //this.SetObjectEsc(player2,0,0,0);
+            this.scene.add(player2);
+        });
+        this.playerControllers.push( new InputMapping(this.keyboard, {
+            'Left': 'A',
+            'Right': 'D',
+            'Up': 'W',
+            'Down': 'S'
+        }));
+
+        this.playerControllers.push(new InputMapping(this.keyboard, {
+            'Left': 'numpad4',
+            'Right': 'numpad6',
+            'Up': 'numpad8',
+            'Down': 'numpad2'
+        }));
     }
+
+    input(){
+        for (let player of this.players) {
+            //this.playerControllers[player.playerId] || 
+            let controller = this.defaultController;
+            player.input(controller);
+        }
+        if(this.keyboard.pressed('esc')){
+            if(!this.paused)
+                this.pause();
+            else
+                this.unpause();
+            this.keyboard.pressed('esc', false);
+        }
+    }
+
+    render(){
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    // pause(){
+    //     if(!this.paused){
+    //         this.paused = !this.paused;
+    //         this.clock.stop();
+    //         this.onGamePaused();
+    //     }
+    // }
+
+    // unpause(){
+    //     if(this.paused){
+    //         this.paused = !this.paused;
+    //         this.clock.start();
+    //         this.onGameUnpaused();
+    //     }
+    // }
 
     _Loop(){
-        deltaTime = this.clock.getDelta();	
-
-        let yaw = 0;
-        let forward = 0;
-        if (keys["W"]) {
-            forward = -20;
-        } else if (keys["S"]) {
-            forward = 20;
+        this.clock.start();
+        let elapsedTime = 0;
+        let self = this;
+        function _Loop_() {
+            let dt = self.clock.getDelta();
+            let yaw = 0;
+            let forward = 0;
+            if (keys["W"]) {
+                forward = -20;
+            }
+            else if (keys["S"]) {
+                forward = 20;
+            }
+            self.camera.rotation.y += yaw * dt;
+            self.camera.translateZ(forward * dt);
+            self.render();
+            requestAnimationFrame(_Loop_);
         }
-
-        if (isWorldReady[0] && isWorldReady[1]) {
-            
-            this.camera.rotation.y += yaw * deltaTime;
-            this.camera.translateZ(forward * deltaTime);
-        }
-    
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(_Loop);
+        requestAnimationFrame(_Loop_);
     }
 
-    onKeyDown(event) {
+    onKeyDown(event){
 		keys[String.fromCharCode(event.keyCode)] = true;
-	}
-	onKeyUp(event) {
+    }
+    
+	onKeyUp(event){
 		keys[String.fromCharCode(event.keyCode)] = false;
 	}
 
-    _setupScene() {		
+    _setupScene(){
         this.camera.position.z = 2;
         this.camera.position.y = 50;
         this.camera.rotation.x = THREE.Math.degToRad(-90);
@@ -75,7 +134,7 @@ class Player{
         let ambientLight = new THREE.AmbientLight(new THREE.Color(1, 1, 1), 1.0);
         this.scene.add(ambientLight);
 
-        let directionalLight = new THREE.DirectionalLight(new THREE.Color(1, 1, 0), 0.4);
+        let directionalLight = new THREE.DirectionalLight(new THREE.Color(0, .5, 1), 0.4);
         directionalLight.position.set(0, 0, 1);
         this.scene.add(directionalLight);
 
@@ -84,36 +143,34 @@ class Player{
         this.scene.add(grid);
     }
     
-    SetObjectPos(object,PosX,PosY,PosZ)
-    {
+    SetObjectPos(object,PosX,PosY,PosZ){
         object.position.x = PosX;
         object.position.y = PosY;
         object.position.z = PosZ;
     }
-	SetObjectRot(object,RotX,RotY,RotZ)
-    {
+
+	SetObjectRot(object,RotX,RotY,RotZ){
 		object.rotation.x = THREE.Math.degToRad(RotX);
 		object.rotation.y = THREE.Math.degToRad(RotY);
 		object.rotation.z = THREE.Math.degToRad(RotZ);
-	}
-	SetObjectEsc(object,EscX,EscY,EscZ)
-    {
+    }
+    
+	SetObjectEsc(object,EscX,EscY,EscZ){
 		object.scale.x = EscX;
 		object.scale.y = EscY;
 		object.scale.z = EscZ;
     }
-    loadOBJWithMTL(path, objFile, mtlFile, onLoadCallback) {
+
+    loadOBJWithMTL(path, objFile, mtlFile, onLoadCallback){
 		let mtlLoader = new THREE.MTLLoader();
 		mtlLoader.setPath(path);
 		mtlLoader.load(mtlFile, (materials) => {
-			
 			let objLoader = new THREE.OBJLoader();
 			objLoader.setMaterials(materials);
 			objLoader.setPath(path);
 			objLoader.load(objFile, (object) => {
 				onLoadCallback(object);
 			});
-
         });
     }
 }
